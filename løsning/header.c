e7a828a388534fc9b2346479503ff7270a7b1c67
Frederik Lund <frederik.bl@live.dk>
2014-11-26 12:57:41 +0100

    header til løsning indtil videre

---
+++ løsning/header.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define ANTAL_ATTRAKTIONER 10
+#define ANTAL_KANTER ANTAL_ATTRAKTIONER*(ANTAL_ATTRAKTIONER-1)/2
+
+typedef struct {
+  char *navn;
+  char *adresse; 
+  double lndg, brdg;
+  int antalKanter;
+} attraktion;
+
+typedef struct {
+  double laengde;
+  attraktion start, slut;
+} kant;

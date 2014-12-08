e2091c592737b50d1a4f9afdff61b84eda8b6ed9
Mark Kloch Haurum <mark@markhaurum.com>
2014-12-08 15:22:34 +0100

    Ændre startsted

--- løsning/beregn_dist.c
+++ løsning/beregn_dist.c
@@ -6,7 +6,7 @@
 #define JORDENS_RADIUS 6373
 #define PI 3.141592653589793
 #define GRAD_TIL_RAD 0.017453292519943295769236907684886
-#define ANTAL_ATTRAKTIONER 5
+#define ANTAL_ATTRAKTIONER 6
 #define ANTAL_KANTER (ANTAL_ATTRAKTIONER*(ANTAL_ATTRAKTIONER-1)/2)
 #define MAX_STRING 30
 
@@ -25,6 +25,7 @@ typedef struct {
 double beregn_dist(double lat1, double lon1, double lat2, double lon2);
 void udregn_kanter(attraktion *attraktioner, kant *kanter);
 void output_liste(attraktion *attraktioner, int antalAttraktioner);
+void aendre_startsted(attraktion *ruten, attraktion nytStartSted, int antalAttraktioner, attraktion *outputRute);
 
 
 int main()
@@ -33,19 +34,22 @@ int main()
 	attraktion attraktioner[ANTAL_ATTRAKTIONER];
 	attraktioner[0].lndg = 9.875491000000011;
 	attraktioner[0].brdg = 57.041256;
-  strcpy(attraktioner[0].navn, "Mit hus");
+	strcpy(attraktioner[0].navn, "Mit hus");
 	attraktioner[1].lndg = 9.95243800000003;
 	attraktioner[1].brdg = 57.036041;
-  strcpy(attraktioner[1].navn, "Mikael bor her");
+	strcpy(attraktioner[1].navn, "Mikael bor her");
 	attraktioner[2].lndg = 9.675491000000011;
 	attraktioner[2].brdg = 56.041256;
-  strcpy(attraktioner[2].navn, "Soerens lejlighed");
+	strcpy(attraktioner[2].navn, "Soerens lejlighed");
 	attraktioner[3].lndg = 10.005491000000011;
 	attraktioner[3].brdg = 56.541256;
-  strcpy(attraktioner[3].navn, "Random sted");
+	strcpy(attraktioner[3].navn, "Random sted");
 	attraktioner[4].lndg = 11.875491000000011;
 	attraktioner[4].brdg = 58.041256;
-  strcpy(attraktioner[4].navn, "Endnu mere random sted");
+	strcpy(attraktioner[4].navn, "Endnu mere random sted");
+  attraktioner[5].lndg = 9.875491000000011;
+  attraktioner[5].brdg = 57.041256;
+  strcpy(attraktioner[5].navn, "Mit hus");
 
 	udregn_kanter(attraktioner, kanter);
 	printf("antal kanter %i\n", ANTAL_KANTER);
@@ -59,7 +63,15 @@ int main()
 	double dist = beregn_dist(lat1, lon1, lat2, lon2);
 	printf("%f\n", dist);
 
-  output_liste(attraktioner, 5);
+	output_liste(attraktioner, ANTAL_ATTRAKTIONER);
+  
+  attraktion outputRute[ANTAL_ATTRAKTIONER];
+  aendre_startsted(attraktioner, attraktioner[1], ANTAL_ATTRAKTIONER, outputRute);
+
+  for (i = 0; i < ANTAL_ATTRAKTIONER; i++)
+    printf("N %s\n", outputRute[i].navn);
+
+
 	return 0;
 }
 
@@ -121,6 +133,31 @@ void output_liste(attraktion *attraktioner, int antalAttraktioner)
     else 
       fprintf(fp, "\n");
   }
-  
-
 }
+
+void aendre_startsted(attraktion *ruten, attraktion nytStartSted, int antalAttraktioner, attraktion *outputRute)
+{
+	int i = 0, startStedIndex = 0;
+
+	for (i = 0; i < antalAttraktioner; i++)
+	{
+		if (strcmp(ruten[i].navn, nytStartSted.navn) == 0)
+			startStedIndex = i;
+	}
+
+	for (i = 0; i < antalAttraktioner; i++)
+	{
+
+		if (startStedIndex == antalAttraktioner-1)
+    {
+      startStedIndex = 1;
+      outputRute[i] = ruten[0];
+    }
+		else
+    {
+      outputRute[i] = ruten[startStedIndex];
+			startStedIndex++;
+    }
+	}
+}
+
